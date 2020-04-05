26bc7f256871f596637ff0eb8ea0278a8cbfa539
primayoriko <primayoriko@gmail.com>
2020-04-05 23:36:30 +0700

    add gameplay - not final 1

---
+++ src/main/java/com/avatarduel/model/Game.java
@@ -0,0 +1,41 @@
+package  com.avatarduel.model;
+
+import com.avatarduel.model.card.*;
+import com.avatarduel.model.player.Player;
+
+public class Game{
+    private static Game game = new Game();
+    private Player p1, p2;
+
+    private int turnId, phaseNum;
+    private final String[] phase = {"Draw Phase", "Main Phase 1", "Battle Phase", "Main Phase 2", "End Phase"};
+
+    private Game(){}
+
+    public static Game getGame(){
+        return game;
+    }
+
+    public void startGame(String a, List<Card> aC, String b, List<Card> bC){
+        this.p1 = new Player(a, aC);
+        this.p2 = new Player(b, bC);
+        this.turnId = 1;
+        this.phaseNum = 0;
+    }
+
+    public void nextPhase(){
+        this.phaseNum = (this.phaseNum + 1)%5;
+        if(this.phaseNum == 0 )
+            this.turnId = (this.turnId+1)%2;
+    }
+
+    public String getPhaseInfo(){
+        return this.phase[this.phaseNum];
+    }
+
+    public int getTurnInfo(){
+        return this.turnId;
+    }
+
+
+}
--- src/main/java/com/avatarduel/model/player/Player.java
+++ src/main/java/com/avatarduel/model/player/Player.java
@@ -3,6 +3,7 @@ package com.avatarduel.model.player;
 import java.util.List;
 import java.util.Random;
 import com.avatarduel.model.card.*;
+import com.avatarduel.model.constant.Element;
 
 
 public class Player {
@@ -12,17 +13,33 @@ public class Player {
   private int playerId;
   private String name;
   private int health;
-  private int power;
+  private int power[];
   private List<Card> decks;
   private List<Card> cards;
 
-  public Player(String name, int power, List<Card> decks){
+  public Player(String name, List<Card> decks){
     this.name = name;
     this.health = initialPlayerHealth;
-    this.power = power;
     this.playerId = ++countPlayer;
+    this.power = new int[Element.numberElmt];
     this.decks = decks;
     this.moveDecksToCards(numberOfStartingCard);
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int getHealth(){
+    return this.health;
+  }
+
+  public int gerPower(int idx){
+    return this.power[idx];
+  }
+
+  public List<Card> getCards() {
+    return this.cards;
   }
 
   public void moveDecksToCards(int numberOfCard){
