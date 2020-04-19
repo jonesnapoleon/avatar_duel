a11f5e61434e36927c3f935658998c1fb7e0437e
primayoriko <primayoriko@gmail.com>
2020-04-19 22:25:18 +0700

    remove avatarstackpane

--- src/main/java/com/avatarduel/style/AvatarArenaLayout.java
+++ src/main/java/com/avatarduel/style/AvatarArenaLayout.java
@@ -1,10 +1,20 @@
 package com.avatarduel.style;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 
-public class AvatarArenaLayout extends AvatarStackPane {
+
+public class AvatarArenaLayout extends StackPane {
     public AvatarArenaLayout(Node children, final double width, final double height, final Color color){
-        super(children, width, height, color);
+        super(children);
+        this.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
+        this.setPrefHeight(height);
+        this.setPrefWidth(width);
     }
 }
